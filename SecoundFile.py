ed349d3cc8be6e78a83f10bac424840c1ce79b30
ABDIRIZAK AHMED OMAR <rozakqayir2002@gmail.com>
2023-08-10 18:59:52 +0300

    gereksiz dosyalar silindi

--- SecoundFile.py
+++
@@ -1,9 +0,0 @@
-u = 10000
-if u > 20:
-    print ('u 20 den buyuktur')
-    if u == 20 :
-        print ('u 20 ile esittir')
-    else:
-        print ('u 20 ye esit degil')
-else:
-    print ('u 20 den kucuk degil')
